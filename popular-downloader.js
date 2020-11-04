79fe0a5cfb4a402f630857e700e3d2d1b2e6569d
Andrew <kolbe1129@gmail.com>
2020-11-04 03:47:30 -0500

    Ready for Review

--- popular-downloader.js
+++ popular-downloader.js
@@ -6,17 +6,26 @@ const rp = require('request-promise')
 const downloadsPath = path.join(__dirname, '/downloads');
 
 rp('https://www.reddit.com/r/popular.json')
-.then(downloads => {
+    .then(downloads => {
 
-    const list = JSON.parse(downloads);
+        const list = JSON.parse(downloads);
 
-    const myDownloadData = list.data.children.map(post => {
-        console.log(post.data)
+        list.data.children.forEach(post => {
+            const ext = path.extname(post.data.url)
+            if (ext === '.jpg' || ext === '.png') {
+                rp(post.data.url, { encoding: 'base64' }).then(media => {
 
-        const jpg = path.extname
+                    fs.writeFile(path.join(__dirname, `./downloads/${post.data.id}${ext}`), media, { encoding: 'base64' } ,err => {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                    });
+                })
+            }
+        })
     })
-})
-
+    .catch(e => console.log(e));
 
 
 
